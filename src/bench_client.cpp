06d05c66c38b3c65458dee9e38894880c19bc975
Unknown-reader <kirillmedvedev23@gmail.com>
2026-04-08 00:12:27 +0300

    Add basic CMakeLists.txt and stub source files

---
+++ src/bench_client.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Multiplexer Showdown Benchmark Client - Not implemented yet" << "\n";
+
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Multiplexer Showdown Server - Not implemented yet" << "\n";
+
+    return 0;
+}
